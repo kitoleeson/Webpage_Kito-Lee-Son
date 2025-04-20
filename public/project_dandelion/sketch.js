16789050469de4296d6d6031e72cbce41c123d92
Kito <kleeson@ualberta.ca>
2025-04-20 00:03:42 -0600

    project dandelion band scrolling update

--- public/project_dandelion/sketch.js
+++ public/project_dandelion/sketch.js
@@ -1,103 +1,125 @@
+/** @format */
+
 executeProcesses();
 
 let slideIndex = [];
 let slideId = [];
 let slides = [];
+
 async function executeProcesses() {
-  const posts = await fillBlogList();
-  let slide_num = 0;
-  for (let i = 0; i < posts.length; i++)
-    if (posts[i].filenames.length > 1) {
-      slides.push(new Slideshow(slide_num));
-      slide_num++;
-    }
+	const posts = await fillBlogList();
+	let slide_num = 0;
+	for (let i in posts)
+		if (posts[i].filenames.length > 1) {
+			slides.push(new Slideshow(slide_num));
+			slide_num++;
+		}
+	initializeMarqueeScroll();
+}
+
+function initializeMarqueeScroll() {
+	const tracks = document.querySelectorAll(".scroll-track");
+	const speeds = Array.from(tracks).map(() => {
+		const speed = Math.random() * 0.8 + 0.2;
+		const direction = Math.random() < 0.5 ? 1 : -1;
+		return speed * direction;
+	});
+
+	window.addEventListener("scroll", () => {
+		const scrollY = window.scrollY;
+		tracks.forEach((track, index) => {
+			const speed = speeds[index];
+			const offset = scrollY * speed;
+			track.style.transform = `translateX(calc(-50% + ${-offset}px))`;
+		});
+	});
+
+	tracks.forEach((track) => {
+		const original = track.innerHTML;
+		while (track.scrollWidth < window.innerWidth * tracks.length) {
+			track.innerHTML += original;
+		}
+	});
 }
 
 function updateSlides(n, i) {
-  // updates current index of slide in slideIndex[]
-  console.log(i);
-  if (n > 0) slides[i].next();
-  else slides[i].prev();
+	// updates current index of slide in slideIndex[]
+	if (n > 0) slides[i].next();
+	else slides[i].prev();
 }
 
 async function fillBlogList() {
-  const response = await fetch('/posts?type=Dandelion');
-  const post_data = await response.json();
-  console.log('post data:', post_data);
+	const response = await fetch("/posts?type=Dandelion");
+	const post_data = await response.json();
+	console.log("post data:", post_data);
 
-  let slide = 0;
-  post_data.forEach((post, index) => {
-    const paragraphs = post.description.split('\r\n\r\n');
-    addPost(
-      slide,
-      index,
-      post.title,
-      post.artist,
-      post.date,
-      post.filenames,
-      paragraphs
-    );
-    if (post.filenames.length > 1) slide++;
-  });
-  return post_data;
+	let slide = 0;
+	post_data.forEach((post, index) => {
+		const paragraphs = post.description.split("\r\n\r\n");
+		addPost(slide, index, post.title, post.artist, post.date, post.filenames, paragraphs);
+		if (post.filenames.length > 1) slide++;
+	});
+	return post_data;
 }
 
 function addPost(slide, index, title, artist, date, filenames, paragraphs) {
-  const list = document.getElementById('blog_list');
-  // slideshow heavily inspired from:
-  // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_multiple
+	const list = document.getElementById("blog_list");
+	// slideshow heavily inspired from:
+	// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_multiple
 
-  // construct spacer band
-  const band = document.createElement('p');
-  band.className = 'band';
-  band.innerHTML = `
-    i created project dandelion to connect and inspire those who follow me on
-    instagram. it is a way to make the app feel more personal, relatable, and
-    true to myself.
-    <span class="trickster"></span>
-  `;
+	// construct spacer band
+	const band = document.createElement("div");
+	band.className = "scroll-marquee";
+	const track = document.createElement("div");
+	track.className = "scroll-track";
+	track.innerHTML = `
+	<span>i created project dandelion to connect and inspire those who follow me on instagram. it is a way to make the
+		app feel more personal, relatable, and true to myself.</span>
+	`;
+	band.appendChild(track);
+	const clone = band.cloneNode(true);
 
-  // construct image/slideshow
-  let image_content = '';
-  if (filenames.length > 1) {
-    image_content += `<div class="slideshow-container">`;
-    filenames.forEach((n) => {
-      image_content += `<div class="slideshow${slide}" style="display: none">\n<img src="images/posts/${n}" alt="album cover or corresponding content" width="300px">\n</div>\n`;
-    });
-    image_content += `<a class="prev" onclick="updateSlides(-1, ${slide})">&#10094;</a>\n<a class="next" onclick="updateSlides(1, ${slide})">&#10095;</a>\n<div class="dots">\n`;
-    filenames.forEach((n) => {
-      image_content += `<span class="dot"></span>\n`;
-    });
-    image_content += '</div>\n</div>';
-  } else if (filenames.length == 1)
-    image_content += `<img src="images/posts/${filenames[0]}" alt="album cover" width="300px" />`;
+	// construct image/slideshow
+	let image_content = "";
+	if (filenames.length > 1) {
+		image_content += `<div class="slideshow-container">`;
+		filenames.forEach((n) => {
+			image_content += `<div class="slideshow${slide}" style="display: none">\n<img src="images/posts/${n}" alt="album cover or corresponding content" width="300px">\n</div>\n`;
+		});
+		image_content += `<a class="prev" onclick="updateSlides(-1, ${slide})">&#10094;</a>\n<a class="next" onclick="updateSlides(1, ${slide})">&#10095;</a>\n<div class="dots">\n`;
+		filenames.forEach((n) => {
+			image_content += `<span class="dot"></span>\n`;
+		});
+		image_content += "</div>\n</div>";
+	} else if (filenames.length == 1)
+		image_content += `<img src="images/posts/${filenames[0]}" alt="album cover" width="300px" />`;
 
-  // construct description
-  let description_content = '';
-  paragraphs.forEach((p) => {
-    description_content += `<p>\n${p}\n</p>\n`;
-  });
+	// construct description
+	let description_content = "";
+	paragraphs.forEach((p) => {
+		description_content += `<p>\n${p}\n</p>\n`;
+	});
 
-  // construct combo of image and description
-  let combo;
-  if (index % 2 == 0)
-    combo = `
+	// construct combo of image and description
+	let combo;
+	if (index % 2 == 0)
+		combo = `
     ${image_content}
     <div class="songdescription">
       ${description_content}
     </div>
   `;
-  else
-    combo = `
+	else
+		combo = `
     <div class="songdescription">
       ${description_content}
     </div>
     ${image_content}
   `;
 
-  const post = document.createElement('div');
-  post.className = 'blog_post';
-  post.innerHTML = `
+	const post = document.createElement("div");
+	post.className = "blog_post";
+	post.innerHTML = `
     <div class="blog_title">
       <div class="artist">
         <img
@@ -118,33 +140,34 @@ function addPost(slide, index, title, artist, date, filenames, paragraphs) {
     </div>
   `;
 
-  list.appendChild(band);
-  list.appendChild(post);
+	list.appendChild(band);
+	list.appendChild(clone);
+	list.appendChild(post);
 }
 
 class Slideshow {
-  constructor(i) {
-    this.index = i;
-    this.slides = document.getElementsByClassName(`slideshow${i}`);
-    this.dots = document.getElementsByClassName('dots')[i].children;
-    this.max = this.dots.length - 1;
-    this.current_slide = 0;
-    this.changeSlide(0); // initialize active dot
-  }
+	constructor(i) {
+		this.index = i;
+		this.slides = document.getElementsByClassName(`slideshow${i}`);
+		this.dots = document.getElementsByClassName("dots")[i].children;
+		this.max = this.dots.length - 1;
+		this.current_slide = 0;
+		this.changeSlide(0); // initialize active dot
+	}
 
-  next = () => this.changeSlide(1);
+	next = () => this.changeSlide(1);
 
-  prev = () => this.changeSlide(-1);
+	prev = () => this.changeSlide(-1);
 
-  changeSlide(n) {
-    this.dots[this.current_slide].className = 'dot';
-    this.slides[this.current_slide].style.display = 'none';
-    this.current_slide += n;
-    if (this.current_slide > this.max) this.current_slide = 0;
-    if (this.current_slide < 0) this.current_slide = this.max;
-    this.dots[this.current_slide].className = 'dot dot-active';
-    this.slides[this.current_slide].style.display = 'block';
-    if (n > 0) console.log(`slide ${this.index} :: ${this.current_slide}`);
-    else console.log(`slide ${this.index} :: ${this.current_slide}`);
-  }
+	changeSlide(n) {
+		this.dots[this.current_slide].className = "dot";
+		this.slides[this.current_slide].style.display = "none";
+		this.current_slide += n;
+		if (this.current_slide > this.max) this.current_slide = 0;
+		if (this.current_slide < 0) this.current_slide = this.max;
+		this.dots[this.current_slide].className = "dot dot-active";
+		this.slides[this.current_slide].style.display = "block";
+		// if (n > 0) console.log(`slide ${this.index} :: ${this.current_slide}`);
+		// else console.log(`slide ${this.index} :: ${this.current_slide}`);
+	}
 }
--- server.js
+++ server.js
@@ -1,77 +1,74 @@
-const sb = require('./server_blog');
-const sc = require('./server_coords');
+/** @format */
 
-const express = require('express');
-const cors = require('cors');
-const Datastore = require('nedb');
-const multer = require('multer');
-const sort = require('sort-array');
-const chalk = require('chalk');
-const path = require('path');
+const sb = require("./server_blog");
+const sc = require("./server_coords");
+
+const express = require("express");
+const cors = require("cors");
+const Datastore = require("nedb");
+const multer = require("multer");
+const sort = require("sort-array");
+const chalk = require("chalk");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
 const app = express();
 initializeApp(app);
 
 /* COORDINATE DATABASE API */
-const coordinate_base = new Datastore('coord_data.db');
+const coordinate_base = new Datastore("coord_data.db");
 coordinate_base.loadDatabase();
 const coord_args = {
-  ch: chalk,
-  db: coordinate_base,
-  st: sort,
+	ch: chalk,
+	db: coordinate_base,
+	st: sort,
 };
 
-app.post('/coords', (request, response) => {
-  sc.post_coords(request, response, coord_args);
+app.post("/coords", (request, response) => {
+	sc.post_coords(request, response, coord_args);
 }); // add new coordinates
-app.get('/coords', (request, response) => {
-  sc.get_coords(request, response, coord_args);
+app.get("/coords", (request, response) => {
+	sc.get_coords(request, response, coord_args);
 }); // get all coordinates
 
 /* BLOG DATABASE API */
-const blog_base = new Datastore('blogs_data.db');
+const blog_base = new Datastore("blogs_data.db");
 blog_base.loadDatabase();
 const blog_args = {
-  ch: chalk,
-  db: blog_base,
-  st: sort,
+	ch: chalk,
+	db: blog_base,
+	st: sort,
 };
 
 let post_count = 0;
-app.post('/new_post', (request, response) => {
-  let filenames = [];
-  const data = request.body;
-  data.type = data.location ? 'Blog' : 'Dandelion';
+app.post("/new_post", (request, response) => {
+	let filenames = [];
+	const data = request.body;
+	data.type = data.location ? "Blog" : "Dandelion";
 
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      if (data.type == 'Blog')
-        cb(null, path.join(__dirname, '/public/trinidad_blog/images/posts'));
-      else
-        cb(
-          null,
-          path.join(__dirname, '/public/project_dandelion/images/posts')
-        );
-    },
-    filename: function (req, file, cb) {
-      req.body.date = req.body.date.replaceAll('-', '.');
-      const name = `${req.body.date}_${post_count}-${filenames.length}.jpg`;
-      filenames.push(name);
-      cb(null, name);
-    },
-  });
-  const upload = multer({ storage: storage });
+	const storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			if (data.type == "Blog") cb(null, path.join(__dirname, "/public/trinidad_blog/images/posts"));
+			else cb(null, path.join(__dirname, "/public/project_dandelion/images/posts"));
+		},
+		filename: function (req, file, cb) {
+			req.body.date = req.body.date.replaceAll("-", ".");
+			const name = `${req.body.date}_${post_count}-${filenames.length}.jpg`;
+			filenames.push(name);
+			cb(null, name);
+		},
+	});
+	const upload = multer({ storage: storage });
 
-  upload.array('images')(request, response, (error) => {
-    if (error) return response.status(500).json(error);
-    blog_args.filenames = filenames;
-    sb.new_post(request, response, blog_args);
-    post_count++;
-  });
+	upload.array("images")(request, response, (error) => {
+		if (error) return response.status(500).json(error);
+		blog_args.filenames = filenames;
+		sb.new_post(request, response, blog_args);
+		post_count++;
+	});
 }); // add new post
-app.get('/posts', (request, response) => {
-  sb.get_post(request, response, blog_args);
+app.get("/posts", (request, response) => {
+	sb.get_post(request, response, blog_args);
 }); // get all posts
 
 // make a "my community" page where people can talk amongst themselves
@@ -80,10 +77,8 @@ app.get('/posts', (request, response) => {
 // fix the server for the blog posts
 
 function initializeApp(app) {
-  app.listen(port, () =>
-    console.log(chalk.green.bold(`>> Listening at ${port}`))
-  );
-  app.use(express.static('public'));
-  app.use(cors()); // allow incoming requests from any IP
-  app.use(express.json({ limit: '1mb' }));
+	app.listen(port, () => console.log(chalk.green.bold(`>> Listening at ${port}`)));
+	app.use(express.static("public"));
+	app.use(cors()); // allow incoming requests from any IP
+	app.use(express.json({ limit: "1mb" }));
 }
